704f67850b2a7631929f39b17205df20115f3f9a
Yassoss <yassine.meddiki@gmail.com>
2024-02-09 13:51:55 +0100

    Primo commit

--- Client.java
+++ Client.java
@@ -1,15 +1,13 @@
-import java.net.*;
+55import java.net.*;
 import java.io.*;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
 
 public class Client{
 	public static void main(String[] args) throws IOException{
 		System.out.println("Hello World!");
 		//Istanza della classe "Socket"
 		Socket socket = new Socket();
-		//Assegnazione dell'IP locale, della porta su cui comunicare e del timeout (in millisecondi)
+		//Assegnazione dell'IP locale(127.0.0.1), della porta(5001) su cui comunicare e del timeout(in millisecondi)
    		socket.connect(new InetSocketAddress("127.0.0.1", 5001), 1000);
     		System.out.println("Connessione avvenuta con successo!");
 	}
